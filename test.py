996bf18df1fcf48865e0bb86a0446009bcec28e4
pufik1337 <tp1714@ic.ac.uk>
2018-03-13 11:59:25 +0000

    More fixes

--- test.py
+++ test.py
@@ -13,8 +13,6 @@ from trainer import FasterRCNNTrainer
 from utils import array_tool as at
 from utils.vis_tool import visdom_bbox
 from utils.eval_tool import eval_detection_voc
-from . import util
-
 
 # fix for ulimit
 # https://github.com/pytorch/pytorch/issues/973#issuecomment-346405667
@@ -25,6 +23,37 @@ resource.setrlimit(resource.RLIMIT_NOFILE, (20480, rlimit[1]))
 
 matplotlib.use('agg')
 
+def resize_bbox(bbox, in_size, out_size):
+    """Resize bounding boxes according to image resize.
+
+    The bounding boxes are expected to be packed into a two dimensional
+    tensor of shape :math:`(R, 4)`, where :math:`R` is the number of
+    bounding boxes in the image. The second axis represents attributes of
+    the bounding box. They are :math:`(y_{min}, x_{min}, y_{max}, x_{max})`,
+    where the four attributes are coordinates of the top left and the
+    bottom right vertices.
+
+    Args:
+        bbox (~numpy.ndarray): An array whose shape is :math:`(R, 4)`.
+            :math:`R` is the number of bounding boxes.
+        in_size (tuple): A tuple of length 2. The height and the width
+            of the image before resized.
+        out_size (tuple): A tuple of length 2. The height and the width
+            of the image after resized.
+
+    Returns:
+        ~numpy.ndarray:
+        Bounding boxes rescaled according to the given image shapes.
+
+    """
+    bbox = bbox.copy()
+    y_scale = float(out_size[0]) / in_size[0]
+    x_scale = float(out_size[1]) / in_size[1]
+    bbox[:, 0] = y_scale * bbox[:, 0]
+    bbox[:, 2] = y_scale * bbox[:, 2]
+    bbox[:, 1] = x_scale * bbox[:, 1]
+    bbox[:, 3] = x_scale * bbox[:, 3]
+    return bbox
 
 def eval(dataloader, faster_rcnn, trainer, test_num=1):
     pred_bboxes, pred_labels, pred_scores = list(), list(), list()
@@ -41,8 +70,8 @@ def eval(dataloader, faster_rcnn, trainer, test_num=1):
         if ii == test_num: break
         if ii%100 == 0:
                 ori_img_ = inverse_normalize(at.tonumpy(imgs[0]))
-                resized_gt_bbox = util.resize_bbox(gt_bboxes[0], (600, 800), (480, 640))
-                resized_pred_bbox = util.resize_bbox(pred_bboxes_[0], (600, 800), (480, 640))
+                resized_gt_bbox = resize_bbox(gt_bboxes[0], (600, 800), (480, 640))
+                resized_pred_bbox = resize_bbox(pred_bboxes_[0], (600, 800), (480, 640))
                 gt_img = visdom_bbox(ori_img_,
                                      at.tonumpy(resized_gt_bbox),
                                      at.tonumpy(gt_labels_[0]))
